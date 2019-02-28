897d4f3d7b5a3c4a9d8f03e561701b307890aef4
John Helveston <john.helveston@gmail.com>
2019-02-28 07:13:10 -0500

    Fixed partworth powertrain plot
    The coefficient data frame was reversed for "gas" and "electric"

--- code/2.2-partworth_plots.R
+++ code/2.2-partworth_plots.R
@@ -14,59 +14,59 @@ coefs
 # Create data frames for plotting each attribute:
 #   level   = The attribute level (x-axis)
 #   utility = The utility associated with each level (y-axis)
-df_price = data %>% 
-    distinct(level = price) %>% 
+df_price = data %>%
+    distinct(level = price) %>%
     arrange(level) %>%
     mutate(utility = c(0, coefs[c('price_20', 'price_25')]))
-df_fuelEconomy = data %>% 
-    distinct(level = fuelEconomy) %>% 
+df_fuelEconomy = data %>%
+    distinct(level = fuelEconomy) %>%
     arrange(level) %>%
     mutate(utility = c(0, coefs[c('fuelEconomy_25', 'fuelEconomy_30')]))
-df_accelTime = data %>% 
-    distinct(level = accelTime) %>% 
+df_accelTime = data %>%
+    distinct(level = accelTime) %>%
     arrange(level) %>%
     mutate(utility = c(0, coefs[c('accelTime_7', 'accelTime_8')]))
-df_powertrain = data %>% 
-    distinct(level = powertrain) %>% 
+df_powertrain = data %>%
+    distinct(level = powertrain) %>%
     arrange(level) %>%
-    mutate(utility = c(0, coefs[c('powertrain_elec')]))
+    mutate(utility = c(coefs[c('powertrain_elec')], 0))
 
 # Get y-axis upper and lower bounds (plots should have the same y-axis):
-utility = c(df_price$utility, df_fuelEconomy$utility, df_accelTime$utility, 
-            df_powertrain$utility) 
+utility = c(df_price$utility, df_fuelEconomy$utility, df_accelTime$utility,
+            df_powertrain$utility)
 ymin = min(utility)
 ymax = max(utility)
 
 # Plot the utility for each attribute
-plot_price = ggplot(df_price, 
+plot_price = ggplot(df_price,
     aes(x=level, y=utility)) +
     geom_point() +
     scale_y_continuous(limits=c(ymin, ymax)) +
     labs(x='Price ($1000)', y='Utility') +
     theme_bw()
-plot_fuelEconomy = ggplot(df_fuelEconomy, 
-    aes(x=level, y=utility)) +                          
+plot_fuelEconomy = ggplot(df_fuelEconomy,
+    aes(x=level, y=utility)) +
     geom_point() +
     scale_y_continuous(limits=c(ymin, ymax)) +
     labs(x='Fuel Economy (mpg)', y='Utility') +
     theme_bw()
-plot_accelTime = ggplot(df_accelTime, 
+plot_accelTime = ggplot(df_accelTime,
     aes(x=level, y=utility)) +
     geom_point() +
     scale_y_continuous(limits=c(ymin, ymax)) +
     labs(x='0-60mph Accel. Time (sec)', y='Utility') +
     theme_bw()
-plot_powertrain = ggplot(df_powertrain, 
+plot_powertrain = ggplot(df_powertrain,
     aes(x=level, y=utility)) +
     geom_point() +
     scale_y_continuous(limits=c(ymin, ymax)) +
-    labs(x='Powertrain Electric', y='Utility') +
+    labs(x='Powertrain', y='Utility') +
     theme_bw()
 
 # Plot all plots in one figure
 multiplot = grid.arrange(plot_price, plot_fuelEconomy, plot_accelTime, plot_powertrain, nrow=1)
 
-# Save plots 
+# Save plots
 mainwd = getwd()
 setwd('./results/plots/coefficients')
 ggsave('./partworth_price.pdf', plot_price, width=4, height=3)
